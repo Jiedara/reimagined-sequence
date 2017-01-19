8506bcfc3943542f19914742b2eb8cfbe3457084
Jiedara <m.villanove@laposte.net>
2017-01-19 15:54:15 -0500

    grid move + matrice generation

--- js/game.js
+++ js/game.js
@@ -1,21 +1,60 @@
-function generateGrid(target, width, height){
+function generateGrid(width, height){
 
 	//initiate var
 	var grid = "";
-	var aside = target.siblings('aside');
+	var movable = "";
 
 	//construct grid
-	for(var i = 0; i <= height; i++ ){
+	for(var i = 1; i <= height; i++ ){
+		matrice[i] = [];
 		grid += "<tr>";
 		for(var j = 1; j <= width; j++ ){
 			grid += "<td></td>";
+			matrice[i][j] = "*";
 		}
-		grid += "<tr>";
+		grid += "</tr>";
 	}
+	
+	movable += "<tr class='movable'>";
+	for(var k = 1; k <= width; k++ ){
+		movable += "<td></td>";
+	}
+	movable += "</tr>";
 
-	//display grid
-	target.append(grid);
+	//display grid and return array representation of grid
+	$('#gameGrid').append(movable + grid );
+}
 
-	//display aside
-	aside.find('.next').append(Math.floor(Math.random() * 3) + 1);
+
+function newNumber(posX){
+	nextNumber = Math.floor(Math.random() * roofNum) + 1
+	$('.item').remove();
+	$('.movable td:nth-child(' + ( posX + 1 ) + ')').html('<span class="item">' + nextNumber + '</span>')
+	return nextNumber;
+}
+
+function dropItem(item, posX){
+
+	var returning = true;
+
+	gridColumn = $('#gameGrid tr:not(".movable") td:nth-child(' + ( posX + 1 ) + ')');
+
+	$.each(matrice, function(key, value){
+		if(matrice != "*" ){
+			//if we are on top of the grid
+			if(key == 1){
+				returning = false;
+			}
+			$(gridColumn[key - 1]).html(item.html());
+			returning = key;
+			return false;
+		}
+		//specific case if the column is empty
+		if( key == matrice.length - 1 && value == "*" ){
+			$(gridColumn[key]).html(item.html());
+			returning = key + 1;
+			return false;
+		}
+	});
+	return returning;
 }
--- js/script.js
+++ js/script.js
@@ -1,3 +1,52 @@
 $( document ).ready(function() {
-	generateGrid($('#gameGrid'), 6, 10);
+	//game parameters
+	roofNum = 3;
+	score = 0;
+	width = 6;
+	height = 10;
+	matrice = [];
+	
+	generateGrid(6, 10);
+	
+	newNumber(1);
+
+	//move item
+	$('html').on('keydown',function(event){
+		item = $('.movable .item');
+		itemHTML = item[0].outerHTML;
+		posX = item.parent().index();
+
+		//controls
+		switch (event.which){
+			//left
+			case 37:
+				if(posX != 0){
+					item.parent().prev().html(itemHTML);
+					item.parent().html('');
+					posX = posX - 1;
+				}
+			break;
+
+			//right
+			case 39:
+				if(posX != width - 1){
+					item.parent().next().html(itemHTML);
+					item.parent().html('');
+					posX = posX + 1;
+				}
+			break;
+
+			//dropping an item
+			case 40:
+				if(posY = dropItem(item,posX)){
+					calcPosX = posX + 1;
+					checkBlock({'posX' : calcPosX,posY});
+					newNumber(posX);
+				}else {
+					lose();
+				}
+			break;
+		}
+	});
+
 });
